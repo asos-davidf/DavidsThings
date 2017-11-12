a6c9d050604612a9f9343d6b0e2c79fdbf8c4b75
David Ferrand <david.ferrand@asos.com>
2017-11-12 21:21:21 +0000

    Take a picture and send it when receiving "Camera"

--- rotatingDisplay/src/main/java/com/davidferrand/davidsthing/MainActivity.kt
+++ rotatingDisplay/src/main/java/com/davidferrand/davidsthing/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.davidferrand.davidsthing
 
 import android.app.Activity
+import android.graphics.Bitmap
+import android.media.ImageReader
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Base64
 import android.util.Log
 import com.google.android.things.contrib.driver.button.Button
 import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
@@ -11,10 +16,11 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.io.ByteArrayOutputStream
 import java.text.Normalizer
 
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), ImageReader.OnImageAvailableListener {
     private lateinit var display: AlphanumericDisplay
     private lateinit var buttonA: Button
     private lateinit var buttonB: Button
@@ -22,9 +28,20 @@ class MainActivity : Activity() {
 
     private lateinit var displayAnimator: DisplayAnimator
 
+    private lateinit var mImagePreprocessor: ImagePreprocessor
+    private var mCameraHandler: CameraHandler? = null
+
+    private var mBackgroundThread: HandlerThread? = null
+    private var mBackgroundHandler: Handler? = null
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    private val database: FirebaseDatabase by lazy { FirebaseDatabase.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupPeriphs()
+        initCamera()
 
         displayAnimator = DisplayAnimator(display)
 
@@ -34,7 +51,6 @@ class MainActivity : Activity() {
 //        playMessageOnButtonPress(buttonB, "Hola")
 //        playMessageOnButtonPress(buttonC, "Bonjour")
 
-        val database = FirebaseDatabase.getInstance()
         val messageRef = database.getReference("message")
 
         messageRef.addValueEventListener(object : ValueEventListener {
@@ -55,8 +71,6 @@ class MainActivity : Activity() {
         button.setOnButtonEventListener { _, pressed -> if (pressed) displayMessage(message) }
     }
 
-    private val TAG = MainActivity::class.java.simpleName
-
     private fun displayMessage(message: String) {
         val normalisedMsg = Normalizer.normalize(message, Normalizer.Form.NFD)
                 .replace(Regex.fromLiteral("[^\\p{ASCII}]"), "")
@@ -64,16 +78,72 @@ class MainActivity : Activity() {
 
         Log.v(TAG, "Message received: '$message'. Normalised: '$normalisedMsg'")
 
-        try {
-            displayAnimator.display(normalisedMsg)
-        } catch (e: Exception) {
-            displayMessage("Invalid message")
+        if (normalisedMsg.equals("camera", ignoreCase = true)) {
+            takePictureAndUpload()
+        } else {
+            try {
+                displayAnimator.display(normalisedMsg)
+            } catch (e: Exception) {
+                displayMessage("Invalid message")
+            }
         }
+    }
+
+    private fun takePictureAndUpload() {
+        mBackgroundHandler?.post(mBackgroundClickHandler)
+    }
+
+    private fun initCamera() {
+        mBackgroundThread = HandlerThread("BackgroundThread").apply { start() }
+        mBackgroundHandler = Handler(mBackgroundThread!!.looper).apply { post(mInitializeOnBackground) }
+    }
+
+    private val mInitializeOnBackground = Runnable {
+        mImagePreprocessor = ImagePreprocessor()
+
+        mCameraHandler = CameraHandler.getInstance()
+        mCameraHandler!!.initializeCamera(
+                this, mBackgroundHandler,
+                this)
+    }
+
+    private val mBackgroundClickHandler = Runnable { mCameraHandler!!.takePicture() }
+
+    override fun onImageAvailable(reader: ImageReader) {
+        var bitmap: Bitmap? = null
+        reader.acquireNextImage().use { image -> bitmap = mImagePreprocessor.preprocessImage(image) }
+
+        bitmap?.let { database.getReference("image").setValue(bitmapToString(it)) }
+    }
+
+    private fun bitmapToString(bitmap: Bitmap): String {
+        val ByteStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, ByteStream)
+        val b = ByteStream.toByteArray()
+        return Base64.encodeToString(b, Base64.DEFAULT)
     }
 
     override fun onDestroy() {
         destroyPeriphs()
+        destroyCamera()
         super.onDestroy()
+    }
+
+    private fun destroyCamera() {
+        try {
+            mBackgroundThread?.quit()
+        } catch (t: Throwable) {
+            // close quietly
+        }
+
+        mBackgroundThread = null
+        mBackgroundHandler = null
+
+        try {
+            mCameraHandler?.shutDown()
+        } catch (t: Throwable) {
+            // close quietly
+        }
     }
 
     private fun setupPeriphs() {
--- rotatingDisplayCompanion/src/main/java/com/davidferrand/rotatingdisplaycompanion/MainActivity.kt
+++ rotatingDisplayCompanion/src/main/java/com/davidferrand/rotatingdisplaycompanion/MainActivity.kt
@@ -1,24 +1,59 @@
 package com.davidferrand.rotatingdisplaycompanion
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val database: FirebaseDatabase by lazy { FirebaseDatabase.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener { uploadMessage(editText.text.toString()) }
+
+        listenToImage()
+    }
+
+    private fun listenToImage() {
+        database.getReference("image").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                displayImage(bitmapFromString(value))
+            }
+
+            override fun onCancelled(p0: DatabaseError?) {
+            }
+        })
+    }
+
+    private fun displayImage(value: Bitmap?) {
+        image.setImageBitmap(value)
+    }
+
+    fun bitmapFromString(encodedString: String?): Bitmap? {
+        return try {
+            val encodeByte = Base64.decode(encodedString, Base64.DEFAULT)
+            BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
+        } catch (e: Exception) {
+            e.message
+            null
+        }
+
     }
 
     private fun uploadMessage(message: String) {
-        val database = FirebaseDatabase.getInstance()
         val messageRef = database.getReference("message")
-
         messageRef.setValue(message)
     }
 }
